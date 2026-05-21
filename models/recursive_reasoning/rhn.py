16d7e262a9e61633db6168710f24a4d37f84d011
coffmaca <coffmaca@oregonstate.edu>
2026-05-21 10:54:38 -0600

    Enabled full BPTT.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -474,52 +474,52 @@ class RHN_ACTV1_Inner(nn.Module):
         # total_kl = torch.zeros(z_L.shape[0], device=z_L.device, dtype=z_L.dtype)
 
         # H_cycles-1 without grad
-        with torch.no_grad():
-            for _H_step in range(self.config.H_cycles-1):
-                for _L_step in range(self.config.L_cycles):
-                    prev_z_L = z_L
-                    z_L, _, step_m = self._dynamic_forward(z_L=z_L,
-                                                z_H=z_H,
-                                                input_embeddings=input_embeddings,
-                                                log_deep_metrics=log_deep_metrics,
-                                                iteration_steps=current_steps,
-                                                **seq_info)
-                    track_metrics(prev_z_L, z_L, step_m)
-                    current_steps += 1
-                prev_z_H = z_H
-                z_H, _, step_m = self._dynamic_forward(z_L=z_L,
+        # with torch.no_grad():
+        for _H_step in range(self.config.H_cycles): #-1):
+            for _L_step in range(self.config.L_cycles):
+                prev_z_L = z_L
+                z_L, step_l2, step_m = self._dynamic_forward(z_L=z_L,
                                             z_H=z_H,
-                                            input_embeddings=None,
+                                            input_embeddings=input_embeddings,
                                             log_deep_metrics=log_deep_metrics,
                                             iteration_steps=current_steps,
                                             **seq_info)
-                track_metrics(prev_z_H, z_H, step_m)
+                track_metrics(prev_z_L, z_L, step_m)
                 current_steps += 1
-
-        for _L_step in range(self.config.L_cycles):
-            prev_z_L = z_L
-            z_L, step_l2, step_m = self._dynamic_forward(z_L=z_L,
+            prev_z_H = z_H
+            z_H, step_l2, step_m = self._dynamic_forward(z_L=z_L,
                                         z_H=z_H,
-                                        input_embeddings=input_embeddings,
+                                        input_embeddings=None,
                                         log_deep_metrics=log_deep_metrics,
                                         iteration_steps=current_steps,
                                         **seq_info)
-            track_metrics(prev_z_L, z_L, step_m)
+            track_metrics(prev_z_H, z_H, step_m)
             current_steps += 1
 
-        prev_z_H = z_H
-        z_H, step_l2, step_m = self._dynamic_forward(z_L=z_L,
-                                    z_H=z_H,
-                                    input_embeddings=None,
-                                    log_deep_metrics=log_deep_metrics,
-                                    iteration_steps=current_steps,
-                                    **seq_info)
-        current_steps += 1
+        # for _L_step in range(self.config.L_cycles):
+        #     prev_z_L = z_L
+        #     z_L, step_l2, step_m = self._dynamic_forward(z_L=z_L,
+        #                                 z_H=z_H,
+        #                                 input_embeddings=input_embeddings,
+        #                                 log_deep_metrics=log_deep_metrics,
+        #                                 iteration_steps=current_steps,
+        #                                 **seq_info)
+        #     track_metrics(prev_z_L, z_L, step_m)
+        #     current_steps += 1
+        #
+        # prev_z_H = z_H
+        # z_H, step_l2, step_m = self._dynamic_forward(z_L=z_L,
+        #                             z_H=z_H,
+        #                             input_embeddings=None,
+        #                             log_deep_metrics=log_deep_metrics,
+        #                             iteration_steps=current_steps,
+        #                             **seq_info)
+        # current_steps += 1
 
         total_l2 += step_l2
         # total_kl += step_kl
 
-        avg_l2 = total_l2 / (self.config.L_cycles + 1)
+        avg_l2 = total_l2 / ((self.config.L_cycles + 1) * self.config.H_cycles)
         # avg_kl = total_kl / (self.config.L_cycles + 1)
 
         track_metrics(prev_z_H, z_H, step_m)
