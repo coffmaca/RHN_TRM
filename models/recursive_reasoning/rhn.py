63b4c088fed0cf284d97c18aba9dbf5d481bf639
coffmaca <coffmaca@oregonstate.edu>
2026-06-23 17:04:00 -0600

    Detached input tensor to hypernet.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -542,7 +542,7 @@ class RHN_ACTV1_Inner(nn.Module):
 
         # Dynamic weight output
         h_dyn = raw_state
-        dynamic_weights, step_l2 = self.hypernet(h_dyn, **seq_info)
+        dynamic_weights, step_l2 = self.hypernet(h_dyn.detach(), **seq_info)
 
         step_metrics = {}
         with torch.no_grad():
