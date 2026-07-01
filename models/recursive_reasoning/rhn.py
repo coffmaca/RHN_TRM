ebbebb8e1978a85485ca806cf6ff3017b0367a00
coffmaca <coffmaca@oregonstate.edu>
2026-07-01 13:28:52 -0600

    Cleanup revision.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -597,7 +597,7 @@ class RHN_ACTV1_Inner(nn.Module):
             h_base = layer(hidden_states=h_base, **seq_info)
 
         hyper_input = torch.cat([raw_state, hyper_enc_expanded], dim=-1)
-        dynamic_weights, step_l2, step_vq = self.hypernet(hyper_input.detach(), **seq_info)
+        dynamic_weights, step_l2, step_vq = self.hypernet(hyper_input.detach(), update_codebook, **seq_info)
 
         step_metrics = {}
         with torch.no_grad():
