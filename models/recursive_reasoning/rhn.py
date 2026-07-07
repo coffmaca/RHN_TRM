1b67f77407d54b08a43c508f998f55a6ef73d62c
coffmaca <coffmaca@oregonstate.edu>
2026-07-06 19:14:44 -0600

    Implemented pre-norm RMSNorm layers for inputs to _attention() prior to passing to MHA module.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -303,11 +303,15 @@ class RHN_Hypernetwork(nn.Module):
 
     def _attention(self, inputs) -> torch.Tensor:
         batch_size = inputs.shape[0]
+
         queries = self.perceiver_queries.expand(batch_size, -1, -1) #.to(dtype=inputs.dtype)
+        norm_queries = rms_norm(queries, variance_epsilon=self.config.rms_norm_eps)
+        norm_inputs = rms_norm(inputs, variance_epsilon=self.config.rms_norm_eps)
+
         attn_output, _ = self.perceiver_attn(
-            query=queries,
-            key=inputs,
-            value=inputs
+            query=norm_queries,
+            key=norm_inputs,
+            value=norm_inputs
         )
 
         return attn_output + queries
