647c24353c247c7ddc486658f8f374157a035361
coffmaca <coffmaca@oregonstate.edu>
2026-07-14 14:20:59 -0600

    Re-added affine parameters to lora_norms and initialized to 0/1 (A/B).

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -299,32 +299,32 @@ class RHN_Hypernetwork(nn.Module):
             if self._is_vector_like(shape):
                 size = shape[0] * self.config.hypernet_rank
                 self.lora_norms[f"{safe_name}"] = nn.RMSNorm(size, eps=self.config.rms_norm_eps,
-                                                             elementwise_affine=False).to(dtype=self.forward_dtype)
+                                                             elementwise_affine=True).to(dtype=self.forward_dtype)
             else:
                 size_a = shape[0] * self.config.hypernet_rank
                 size_b = shape[1] * self.config.hypernet_rank
 
                 self.lora_norms[f"{safe_name}_A"] = nn.RMSNorm(size_a, eps=self.config.rms_norm_eps,
-                                                               elementwise_affine=False).to(dtype=self.forward_dtype)
+                                                               elementwise_affine=True).to(dtype=self.forward_dtype)
                 self.lora_norms[f"{safe_name}_B"] = nn.RMSNorm(size_b, eps=self.config.rms_norm_eps,
-                                                               elementwise_affine=False).to(dtype=self.forward_dtype)
+                                                               elementwise_affine=True).to(dtype=self.forward_dtype)
 
         # with torch.no_grad():
         #     # target_variance = 1.0 / self.config.hidden_size
         #     # symmetric_std = (target_variance / self.config.hypernet_rank) ** 0.25
-        #     for key, norm_module in self.lora_norms.items():
+            for key, norm_module in self.lora_norms.items():
         #         trunc_normal_init_(norm_module.weight, std=0.02)
                 # norm_module.weight.add_(1.0)
 
                 # trunc_normal_init_(norm_module.weight, std=symmetric_std)
                 # norm_module.weight *= 10
 
-                # if key.endswith("_B"):
-                #     # Initialize B matrices to 0.0 so dynamic output starts safely at zero
-                #     nn.init.zeros_(norm_module.weight)
-                # else:
-                #     # Initialize A matrices (and vectors) to 1.0 unit variance
-                #     nn.init.ones_(norm_module.weight)
+                if key.endswith("_B"):
+                    # Initialize B matrices to 0.0 so dynamic output starts safely at zero
+                    nn.init.zeros_(norm_module.weight)
+                else:
+                    # Initialize A matrices (and vectors) to 1.0 unit variance
+                    nn.init.ones_(norm_module.weight)
 
     def forward(self, activations: torch.Tensor, **seq_info) -> Tuple[dict, torch.Tensor]:
         batch_size, seq_len, _ = activations.shape
