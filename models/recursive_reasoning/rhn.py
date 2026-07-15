Revised lora_norm initializations to fix 0 gradient to hypernetwork.

models/recursive_reasoning/rhn.py
@@ -306,9 +306,9 @@ class RHN_Hypernetwork(nn.Module):
                 self.lora_norms[f"{safe_name}_B"] = nn.RMSNorm(size_b, eps=self.config.rms_norm_eps,
                                                                elementwise_affine=True).to(dtype=self.forward_dtype)
 
-        # with torch.no_grad():
-        #     # target_variance = 1.0 / self.config.hidden_size
-        #     # symmetric_std = (target_variance / self.config.hypernet_rank) ** 0.25
+        with torch.no_grad():
+            target_variance = 1.0 / self.config.hidden_size
+            symmetric_std = (target_variance / self.config.hypernet_rank) ** 0.25
             for key, norm_module in self.lora_norms.items():
         #         trunc_normal_init_(norm_module.weight, std=0.02)
                 # norm_module.weight.add_(1.0)
@@ -318,7 +318,8 @@ class RHN_Hypernetwork(nn.Module):
 
                 if key.endswith("_B"):
                     # Initialize B matrices to 0.0 so dynamic output starts safely at zero
-                    nn.init.zeros_(norm_module.weight)
+                    # nn.init.zeros_(norm_module.weight)
+                    trunc_normal_init_(norm_module.weight, std=symmetric_std)
                 else:
                     # Initialize A matrices (and vectors) to 1.0 unit variance
                     nn.init.ones_(norm_module.weight)