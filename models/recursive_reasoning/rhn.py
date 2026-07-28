778a35e123e485b264f7ae00ec53b80e24b371e0
coffmaca <coffmaca@oregonstate.edu>
2026-07-28 15:24:06 -0600

    Converted tanh scalar from output scalar to input scalar.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -226,7 +226,7 @@ class RHN_Hypernetwork(nn.Module):
                                          self._output_dim(layer_specs),
                                          bias=False)
 
-        self.post_output_head_scalar = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
+        self.tanh_scalar_in = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
 
     def forward(self, activations: torch.Tensor) -> Tuple[dict, torch.Tensor]:
         batch_size, seq_len, _ = activations.shape
@@ -238,7 +238,7 @@ class RHN_Hypernetwork(nn.Module):
         outputs = self.hypernet_base(inputs)
         outputs = self.output_head(outputs)
         # outputs = rms_norm(outputs, variance_epsilon=self.config.rms_norm_eps)
-        outputs = torch.tanh(outputs) * self.post_output_head_scalar
+        outputs = torch.tanh(outputs * self.tanh_scalar_in)
         outputs = self._expand_output(outputs)
 
         step_l2 = outputs.view(batch_size, -1).pow(2).sum(dim=1)
