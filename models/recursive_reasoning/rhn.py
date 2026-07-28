677a05c7545a147e1e0e85fb39f853f64e0d7f20
coffmaca <coffmaca@oregonstate.edu>
2026-07-28 12:08:19 -0600

    Removed learned scalars.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -226,9 +226,9 @@ class RHN_Hypernetwork(nn.Module):
                                          self._output_dim(layer_specs),
                                          bias=False)
 
-        self.post_output_head_scalar_a = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
-
-        self.post_output_head_scalar_b = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
+        # self.post_output_head_scalar_a = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
+        #
+        # self.post_output_head_scalar_b = nn.Parameter(torch.full((1,), 1, dtype=self.forward_dtype))
 
     def forward(self, activations: torch.Tensor) -> Tuple[dict, torch.Tensor]:
         batch_size, seq_len, _ = activations.shape
@@ -322,10 +322,10 @@ class RHN_Hypernetwork(nn.Module):
         outputs = outputs.reshape(batch_size, -1)  # Collapse perceiver rank dimension
         used_outputs_a = outputs[..., :self.kron_dim ** 2]
         used_outputs_a = used_outputs_a.unsqueeze(-1).view(-1, self.kron_dim, self.kron_dim)
-        used_outputs_a = torch.tanh(used_outputs_a) * self.post_output_head_scalar_a
+        used_outputs_a = torch.tanh(used_outputs_a) # * self.post_output_head_scalar_a
         used_outputs_b = outputs[..., self.kron_dim ** 2: self.kron_dim ** 2 * 2]
         used_outputs_b = used_outputs_b.unsqueeze(-1).view(-1, self.kron_dim, self.kron_dim)
-        used_outputs_b = torch.tanh(used_outputs_b) * self.post_output_head_scalar_b
+        used_outputs_b = torch.tanh(used_outputs_b) # * self.post_output_head_scalar_b
         expanded_outputs = torch.einsum('bij,bkl->bikjl', used_outputs_a, used_outputs_b)
         outputs = expanded_outputs.flatten(start_dim=1, end_dim=-1)
 
