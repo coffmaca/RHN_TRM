86b05c33dfb1740cd6a3898af4ddb1dfa9d228b6
coffmaca <coffmaca@oregonstate.edu>
2026-07-28 19:59:19 -0600

    Removed post-expansion rms layers.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -255,11 +255,11 @@ class RHN_Hypernetwork(nn.Module):
                 output_index += shape[1] * self.config.hypernet_rank
 
             if self.config_per_layer[layer]["type"] == "vector":
-                outputs_a = rms_norm(outputs_a, variance_epsilon=self.config.rms_norm_eps)
+                # outputs_a = rms_norm(outputs_a, variance_epsilon=self.config.rms_norm_eps)
                 outputs_by_layer[layer] = outputs_a
             else:
-                outputs_a = rms_norm(outputs_a, variance_epsilon=self.config.rms_norm_eps)
-                outputs_b = rms_norm(outputs_b, variance_epsilon=self.config.rms_norm_eps)
+                # outputs_a = rms_norm(outputs_a, variance_epsilon=self.config.rms_norm_eps)
+                # outputs_b = rms_norm(outputs_b, variance_epsilon=self.config.rms_norm_eps)
                 outputs_by_layer[layer] = (outputs_a, outputs_b)
 
         return outputs_by_layer, step_l2
