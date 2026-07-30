167b457667a4e2f34d73323e6715539abe9aecf9
coffmaca <coffmaca@oregonstate.edu>
2026-07-30 15:34:36 -0600

    Added scalar divisor to _expand_output() to nullify the magnitude increase caused by Kronecker product.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -414,6 +414,8 @@ class RHN_Hypernetwork(nn.Module):
         expanded_outputs = torch.einsum('bij,bkl->bikjl', used_outputs_a, used_outputs_b)
         outputs = expanded_outputs.flatten(start_dim=1, end_dim=-1)
 
+        outputs = outputs / self.kron_dim
+
         return outputs
 
 
