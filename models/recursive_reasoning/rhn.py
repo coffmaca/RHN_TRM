b33d348c34c6fe863bd959e83d2098fb9d403a42
coffmaca <coffmaca@oregonstate.edu>
2026-07-30 10:30:43 -0600

    Reduced scale of initial tanh_scalar_in.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -227,7 +227,7 @@ class RHN_Hypernetwork(nn.Module):
                                          self.output_dim,
                                          bias=False)
 
-        self.tanh_scalar_in = nn.Parameter(torch.full((1, self.output_dim), .001, dtype=self.forward_dtype))
+        self.tanh_scalar_in = nn.Parameter(torch.full((1, self.output_dim), .0001, dtype=self.forward_dtype))
 
         # self.tanh_scalar_out = nn.Parameter(torch.full((1, self.output_dim), 1, dtype=self.forward_dtype))
 
