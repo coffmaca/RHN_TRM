847acaca03d1303c030660d84776b5ccaa6b5660
coffmaca <coffmaca@oregonstate.edu>
2026-08-04 21:50:48 -0600

    Re-enabled and updated hypernet_l2_loss.

--- models/losses.py
+++ models/losses.py
@@ -98,13 +98,14 @@ class ACTLossHead(nn.Module):
         lm_loss = (self.loss_fn(outputs["logits"], labels, ignore_index=IGNORE_LABEL_ID, valid_mask=mask) / loss_divisor).sum()
         q_halt_loss = F.binary_cross_entropy_with_logits(outputs["q_halt_logits"], seq_is_correct.to(outputs["q_halt_logits"].dtype), reduction="sum")
 
-        scaled_l2_loss = (outputs["hypernet_l2"] * valid_metrics).sum() * self.l2_lambda
+        scaled_l2_loss = (outputs["hypernet_l2"]).sum() * self.l2_lambda
+        scaled_l2_loss_metric = (outputs["hypernet_l2"] * valid_metrics).sum() * self.l2_lambda
         # scaled_kl_loss = (outputs["hypernet_kl"] * valid_metrics).sum() * self.kl_lambda
 
         metrics.update({
             "lm_loss": lm_loss.detach(),
             "q_halt_loss": q_halt_loss.detach(),
-            "hypernet_l2_loss": scaled_l2_loss.detach(),
+            "hypernet_l2_loss": scaled_l2_loss_metric.detach(),
             # "hypernet_kl_loss": scaled_kl_loss.detach()
         })
         # Q continue (bootstrapping target loss); Alexia: This fits Q-learning, but seems totally unecessary
@@ -116,7 +117,7 @@ class ACTLossHead(nn.Module):
         # Filter outputs for return
         detached_outputs = {k: outputs[k].detach() for k in return_keys if k in outputs}
 
-        final_loss = lm_loss + 0.5 * (q_halt_loss + q_continue_loss) # + scaled_kl_loss
+        final_loss = lm_loss + 0.5 * (q_halt_loss + q_continue_loss) + scaled_l2_loss
 
         return new_carry, final_loss, metrics, detached_outputs, new_carry.halted.all()
 
