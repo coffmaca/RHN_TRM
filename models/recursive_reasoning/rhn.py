e113b9ca81ee3d5be92353b22ccba9214ef85908
coffmaca <coffmaca@oregonstate.edu>
2026-08-06 11:30:32 -0600

    Applied softplus to lora_scalars and added 1.0 before multiplying by ouptut of tanh to ensure scaling always only increases or maintains magnitude.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -243,7 +243,7 @@ class RHN_Hypernetwork(nn.Module):
                                                              eps=self.config.rms_norm_eps,
                                                              elementwise_affine=False).to(dtype=self.forward_dtype)
                 self.lora_scalars[f"{safe_name}"] = nn.Parameter(
-                    torch.ones((1, size_a), dtype=self.forward_dtype)
+                    torch.zeros((1, size), dtype=self.forward_dtype)
                 )
             else:
                 size_a = shape[0] * self.config.hypernet_rank
@@ -256,10 +256,10 @@ class RHN_Hypernetwork(nn.Module):
                                                                eps=self.config.rms_norm_eps,
                                                                elementwise_affine=False).to(dtype=self.forward_dtype)
                 self.lora_scalars[f"{safe_name}_A"] = nn.Parameter(
-                    torch.ones((1, size_a), dtype=self.forward_dtype)
+                    torch.zeros((1, size_a), dtype=self.forward_dtype)
                 )
                 self.lora_scalars[f"{safe_name}_B"] = nn.Parameter(
-                    torch.ones((1, size_b), dtype=self.forward_dtype)
+                    torch.zeros((1, size_b), dtype=self.forward_dtype)
                 )
 
         # with torch.no_grad():
@@ -320,9 +320,9 @@ class RHN_Hypernetwork(nn.Module):
             outputs_a = torch.tanh(outputs_a * 0.5)
 
             if layer_info["type"] == "matrix":
-                outputs_a = self.lora_scalars[f"{safe_name}_A"] * outputs_a
+                outputs_a = (F.softplus(self.lora_scalars[f"{safe_name}_A"]) + 1.0) * outputs_a
             else:
-                outputs_a = self.lora_scalars[f"{safe_name}"] * outputs_a
+                outputs_a = (F.softplus(self.lora_scalars[f"{safe_name}"]) + 1.0) * outputs_a
 
             outputs_a = outputs_a.view(batch_size, shape[0], self.config.hypernet_rank)
 
