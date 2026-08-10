47ba56466609e2dac6b57f2d283965525cc97d6a
coffmaca <coffmaca@oregonstate.edu>
2026-08-10 11:01:14 -0600

    Added residual connection around input attention layer in hypernetwork.

--- models/recursive_reasoning/rhn.py
+++ models/recursive_reasoning/rhn.py
@@ -230,6 +230,7 @@ class RHN_Hypernetwork(nn.Module):
         batch_size, seq_len, _ = activations.shape
 
         inputs = self._attention(activations)
+        inputs = inputs + activations
 
         inputs = rms_norm(inputs, variance_epsilon=self.config.rms_norm_eps)
 
